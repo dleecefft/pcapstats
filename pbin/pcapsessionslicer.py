e3cc7cac8ea3dd8917b88a2d20067616d2ac6d5b
dleecefft <dleece@firstfiretech.ca>
2016-04-13 21:11:16 -0600

    Pcap session splitter with scapy, added output file option

--- pbin/pcapsessionslicer.py
+++ pbin/pcapsessionslicer.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
 
 from scapy.all import *
-import re, sys, getopt
+import re, sys, getopt, shutil
 
-if len(sys.argv) < 4 :
-    print "three args dude "
-    exit()
-
-pcapfile=sys.argv[1]
-pktgrep = sys.argv[2]
-include= sys.argv[3]
-
-include=True
 
 def pcapsessions(pfile):
     pssn = rdpcap(pfile)
@@ -37,14 +28,19 @@ def partstrsplit(ipportstr):
     # stack up the list and split out the port values
     retlist.append(sssnlist[0])
     retlist.append(sssnlist[1])
+    retlist.append('')
     retlist.append(sssnlist[3])
-    #tmpip = sssnlist[1].split(':')
-    #retlist.append(tmpip[0])
-    #retlist.append(tmpip[3])
-    #tmpip = sssnlist[1].split(':')
-    #retlist.append(tmpip[0])
-    #retlist.append(tmpip[1])
+    retlist.append('')
     return retlist
+
+def writesessioncsv(fileline,wfile):
+    try:
+        with open(wfile,'a') as wfh:
+            wfh.write(fileline + "\n")
+    except Exception as e:
+        print(e)
+        pass
+    return
 
 
 def sessionparse(ssnobj,include,pktgrep):
@@ -70,13 +66,49 @@ def sessionparse(ssnobj,include,pktgrep):
                 kline = kline +"," + str(len(v))
                 print kline
         elif not include and rxparse is None:
-            print k,str(len(v))
+            ksplit = partstrsplit(k)
+            kline = ','.join(map(str,ksplit))
+            kline = kline +"," + str(len(v))
+            print kline
+            #print k,str(len(v))
+    return
+
+def sessionparsewrite(ssnobj,include,pktgrep,csvoutfile):
+    sessions = ssnobj.sessions()
+
+    for k, v in sessions.iteritems():
+
+        rxparse = re.match(r'^\w+\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}).*\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5})',k)
+
+        if include and rxparse is not None:
+            # looking for a match
+            if rxparse.group(1) == pktgrep or rxparse.group(2) == pktgrep :
+                ksplit = fullstrsplit(k)
+                kline = ','.join(map(str,ksplit))
+                kline = kline +"," + str(len(v))
+                writesessioncsv(kline,csvoutfile)
+        elif rxparse is not None:
+            if rxparse.group(1) != pktgrep and rxparse.group(2) != pktgrep :
+                #print k,str(len(v))
+                ksplit = fullstrsplit(k)
+                kline = ','.join(map(str,ksplit))
+                kline = kline +"," + str(len(v))
+                writesessioncsv(kline,csvoutfile)
+
+        elif not include and rxparse is None:
+            ksplit = partstrsplit(k)
+            kline = ','.join(map(str,ksplit))
+            kline = kline +"," + str(len(v))
+            writesessioncsv(kline,csvoutfile)
+            #print k,str(len(v))
+    return
 
 
 if __name__ == "__main__":
 
-# option to override certain params if
-    if len(sys.argv) < 2 :
+    if len(sys.argv) > 3 :
+        action=''
+        outcsv=False
         # Use getopt to avoid param order errors
         opts, args = getopt.getopt(sys.argv[1:],"f:m:o:t:h:")
         for o, a in opts:
@@ -86,20 +118,25 @@ if __name__ == "__main__":
                 strmatch=a
             elif o == '-o':
                 outfile=a
+                outcsv=True
             elif o == '-t':
                 action=a
-            elif o == '-h':
-                print("Usage: %s -f file.pcap -m ip:port_string -o [outputfile] " % sys.argv[0])
-                exit()
             else:
                 print("Usage: %s -f file.pcap -m ip:port_string -o [outputfile] " % sys.argv[0])
                 exit()
+    else:
+        print("Usage: %s -f file.pcap -m ip:port_string -o [outputfile] " % sys.argv[0])
+        exit()
+    # default action is search for string provided vs exclude
     if action == "exclude":
         action=False
     else:
-        action == True
+        action=True
 
     # grab sessions from pcap
     thisssnobj = pcapsessions(capfile)
-    sessionparse(thisssnobj,action,strmatch)
+    if outcsv:
+        sessionparsewrite(thisssnobj,action,strmatch,outfile)
+    else:
+        sessionparse(thisssnobj,action,strmatch)
 
